2a1bacc3e3092c896e3802a51946316292f56f8e
Eyob Alemu <it.eyob.alemu@gmail.com>
2021-01-24 22:21:47 +0300

    refactor code

--- assets/js/account.js
+++ assets/js/account.js
@@ -1,24 +1,27 @@
-let account = {
-    balance: 0,
-    withdraw: function (amount) {
-        if (amount < this.balance) {
-            this.balance -= amount;
-            return true;
-        } 
-        return false;
-    },
-    deposit: function (amount) {
-        this.balance += amount;
-        return true;
-    },
-    checkBalance: function () {
-        return this.balance;
-    },
-    transfer: function (amount, to) {
-        if (this.withdraw(amount)) {
-            return `Succesfully transferd ${amount}$ to ${to}!`;
-        } 
-        return "You don't have enough balance!"
-    }
+balance = 0;
 
+const account = {
+    balance: 0,
+    withdraw (amount) {
+        if (amount < balance) {
+            balance -= amount;
+            return [true, `Succesfully Withdrawn ${amount}`];
+        } 
+        return [false, "You don't have enough balance!"];
+    },
+    deposit (amount) {
+        balance += amount;
+        return `Succesfully deposited ${amount}`;
+    },
+    checkBalance () {
+        return balance;
+    },
+    transfer (amount, to) {
+        let check = this.withdraw(amount);
+        if (check[0]) {
+            return `Succesfully transfered ${amount} to account number ${to}`;
+        }
+
+        return check[1];
+    },
 }
